8dc2eb480b657d03f365950eb507b09bfec5ff9c
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-06-29 21:37:47 +0100

    feat: complete initial comments routes

---
+++ comments/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id]);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { content } = req.body;
+  let comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+const PORT = 4001;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}...`);
+});
