199bef1f76a44767118b275d6ffc4700e608c364
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-06-30 07:05:17 +0100

    feat: initialise react files

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Chitter</h1>
+      <hr />
+      <h2>Create Chirp</h2>
+      <hr />
+      <h2>Chirps</h2>
+    </div>
+  );
+}
+
+export default App;
