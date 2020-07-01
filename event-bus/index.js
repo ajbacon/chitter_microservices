d8c6007f614b7f41ec801751fec02419fa01eae8
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-07-01 07:57:27 +0100

    feat: create simple event-bus and link to comments and posts services

---
+++ event-bus/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', async (req, res) => {
+  const event = req.body;
+
+  await axios.post('http://localhost:4000/events', event);
+  await axios.post('http://localhost:4001/events', event);
+  await axios.post('http://localhost:4002/events', event);
+
+  res.send({ status: 'OK' });
+});
+
+const PORT = 4005;
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}...`);
+});
